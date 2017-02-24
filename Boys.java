e2c2ab152d8b7ebcd25c56b891d18aa9b82c2bc2
Mritunjay Chaudhary <mkdd10@gmail.com>
2017-02-25 01:32:13 +0530

    Update Boys.java

--- Boys.java
+++ Boys.java
@@ -2,6 +2,21 @@ public class Boys {
 	String name;
 	int intelligence;
 	int attractivness;
+	int min_attr_req;
 	int budget;
-};
+	String girlf;
+	String status;
+	
+
+
+	boolean is_eligible(int expense,int attar){
+		if (this.budget >= expense && this.min_attr_req <= attar){
+			return true;
+		}else{
+			return false;
+		}
+			
+		}
+	}
+
 
