75ea39fa8d9689644dd26c8b836556cd39a694ad
Mritunjay Chaudhary <mkdd10@gmail.com>
2017-02-25 01:30:53 +0530

    Update q1.java

--- q1.java
+++ q1.java
@@ -1,10 +1,11 @@
 import java.io.*;
 public class q1{
 	public static void main(String args[]){
-		int i;
+		int i,j;
 		i=0;
+		j=0;
 		Boys b[] = new Boys[1000];
-		String csvFile = "/home/jarvis/workspace/my_PPL/src/boys.csv/";
+		String csvFile = "/ppl-assignment-jarvis004/boys.csv/";
 		String line = "";
 		String csvSplit = ",";
 		BufferedReader buff = null;
@@ -17,7 +18,10 @@ public class q1{
 			    b[i].name = boy[0];
 				b[i].intelligence = Integer.parseInt(boy[1]);
 				b[i].attractivness = Integer.parseInt(boy[2]);
-				b[i].budget = Integer.parseInt(boy[3]);
+				b[i].min_attr_req = Integer.parseInt(boy[3]);
+				b[i].budget = Integer.parseInt(boy[4]);
+				b[i].girlf="";
+				b[i].status = boy[5];
 				i++;
 			}
 		}
@@ -29,7 +33,7 @@ public class q1{
 		}
 		
 		Girls g[] = new Girls[1000];
-		csvFile = "/home/jarvis/workspace/my_PPL/src/boys.csv/";
+		csvFile = "/ppl-assignment-jarvis004/girls.csv/";
 		line = "";
 		csvSplit = ",";
 		buff = null;
@@ -40,14 +44,14 @@ public class q1{
 			while((line = buff.readLine() )!= null){
 				String[] girl = line.split(csvSplit);
 			//	System.out.println(boy[0] + boy[1] + boy[2] + boy [3]);
-				g[i] = new Girls();
-			    g[i].name = girl[0];
-				g[i].maintenance = Integer.parseInt(girl[1]);
-				g[i].intelligence = Integer.parseInt(girl[2]);
-				g[i].budget = Integer.parseInt(girl[3]);
-				//g[i].status = girl[4];
-				
-				i++;
+				g[j] = new Girls();
+			    g[j].name = girl[0];
+				g[j].attar = Integer.parseInt(girl[1]);
+				g[j].expense = Integer.parseInt(girl[2]);
+				g[j].intelligence = Integer.parseInt(girl[3]);
+				g[j].status = girl[4];
+				g[j].boyf="";
+				j++;
 			}
 		}
 		catch(FileNotFoundException e){
@@ -63,11 +67,22 @@ public class q1{
 					}
 				}
 		}
-/*		int k;
-		k=0;
-		for(k=0;k<i;k++){
-			System.out.println(b[k].name+","+b[k].intelligence+","+b[k].attractivness+","+b[k].budget);
-			System.out.println(g[k].name+","+g[k].maintenance+","+g[k].intelligence+","+b[k].budget+","+g[k].status);
-		}*/
+		
+	int k,l;
+	for(k=0;k<i;k++){
+		for(l=0;l<j;l++){
+			if(b[k].is_eligible(g[l].expense,g[l].attar) && g[l].is_eligible(b[k].budget) && g[l].status.equals("Single")  && b[k].status.equals("Single") ){
+				b[k].girlf = g[l].name;
+				g[l].boyf = b[k].name;
+				b[k].status = "Is_Committed";
+				g[l].status = "Is_Committed";
+								
+			}
+		}
+			}
+/*	for(k=0;k<i;k++){
+		if(b[k].status.equals("Is_Committed")){
+			System.out.println(b[k].name +" is committed with "+ b[k].girlf + " "+b[k].status);}
+}  */
 	}
 }
