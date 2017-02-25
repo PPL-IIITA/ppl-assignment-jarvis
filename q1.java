5ba92972095140d60b60bb5a5a1bc65cdc1858dc
Mritunjay Chaudhary <mkdd10@gmail.com>
2017-02-26 00:40:19 +0530

    Update q1.java

--- q1.java
+++ q1.java
@@ -1,27 +1,30 @@
 import java.io.*;
 public class q1{
+	
 	public static void main(String args[]){
 		int i,j;
 		i=0;
 		j=0;
 		Boys b[] = new Boys[1000];
-		String csvFile = "/ppl-assignment-jarvis004/boys.csv/";
+		csv_gen csv_files = new csv_gen();
+		csv_files.csv();  
+		String csvFile = "/ppl-assignment-jarvis004/boys.csv";
 		String line = "";
 		String csvSplit = ",";
 		BufferedReader buff = null;
 		try{
 			buff = new BufferedReader(new FileReader(csvFile));
 			while((line = buff.readLine() )!= null){
-				String[] boy = line.split(csvSplit);
+				String[] boys_table = line.split(csvSplit);
 			//	System.out.println(boy[0] + boy[1] + boy[2] + boy [3]);
 				b[i] = new Boys();
-			    b[i].name = boy[0];
-				b[i].intelligence = Integer.parseInt(boy[1]);
-				b[i].attractivness = Integer.parseInt(boy[2]);
-				b[i].min_attr_req = Integer.parseInt(boy[3]);
-				b[i].budget = Integer.parseInt(boy[4]);
+			    b[i].name = boys_table[0];
+				b[i].intelligence = Integer.parseInt(boys_table[1]);
+				b[i].attractivness = Integer.parseInt(boys_table[2]);
+				b[i].min_attr_req = Integer.parseInt(boys_table[3]);
+				b[i].budget = Integer.parseInt(boys_table[4]);
 				b[i].girlf="";
-				b[i].status = boy[5];
+				b[i].status = boys_table[5];
 				i++;
 			}
 		}
@@ -33,7 +36,7 @@ public class q1{
 		}
 		
 		Girls g[] = new Girls[1000];
-		csvFile = "/ppl-assignment-jarvis004/girls.csv/";
+		csvFile = "/ppl-assignment-jarvis004/girls.csv";
 		line = "";
 		csvSplit = ",";
 		buff = null;
@@ -42,14 +45,14 @@ public class q1{
 		try{
 			buff = new BufferedReader(new FileReader(csvFile));
 			while((line = buff.readLine() )!= null){
-				String[] girl = line.split(csvSplit);
+				String[] girls_table = line.split(csvSplit);
 			//	System.out.println(boy[0] + boy[1] + boy[2] + boy [3]);
 				g[j] = new Girls();
-			    g[j].name = girl[0];
-				g[j].attar = Integer.parseInt(girl[1]);
-				g[j].expense = Integer.parseInt(girl[2]);
-				g[j].intelligence = Integer.parseInt(girl[3]);
-				g[j].status = girl[4];
+			    g[j].name = girls_table[0];
+				g[j].attar = Integer.parseInt(girls_table[1]);
+				g[j].expense = Integer.parseInt(girls_table[2]);
+				g[j].intelligence = Integer.parseInt(girls_table[3]);
+				g[j].status = girls_table[4];
 				g[j].boyf="";
 				j++;
 			}
@@ -68,21 +71,27 @@ public class q1{
 				}
 		}
 		
-	int k,l;
-	for(k=0;k<i;k++){
-		for(l=0;l<j;l++){
-			if(b[k].is_eligible(g[l].expense,g[l].attar) && g[l].is_eligible(b[k].budget) && g[l].status.equals("Single")  && b[k].status.equals("Single") ){
-				b[k].girlf = g[l].name;
-				g[l].boyf = b[k].name;
-				b[k].status = "Is_Committed";
-				g[l].status = "Is_Committed";
-								
+		int k,l;
+		String s[] = new String[100];
+		int cnt = 0;
+		for(k=0;k<i;k++){
+			for(l=0;l<j;l++){
+				if(b[k].is_eligible(g[l].expense,g[l].attar) && g[l].is_eligible(b[k].budget) && g[l].status.equals("Single")  && b[k].status.equals("Single") ){
+					b[k].girlf = g[l].name;
+					g[l].boyf = b[k].name;
+					b[k].status = "Is_Committed";
+					g[l].status = "Is_Committed";
+					if(b[k].status.equals("Is_Committed")){
+					s[cnt] = "Boy: "+ b[k].name +" is committed with "+"Girl: "+ b[k].girlf  ;
+					}
+					
+					cnt++;
+									
+				}
+				
 			}
 		}
-			}
-/*	for(k=0;k<i;k++){
-		if(b[k].status.equals("Is_Committed")){
-			System.out.println(b[k].name +" is committed with "+ b[k].girlf + " "+b[k].status);}
-}  */
+		
+		MyLogger.logger(s,cnt);	
 	}
-}
+	}
