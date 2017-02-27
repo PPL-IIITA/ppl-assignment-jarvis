d0aa5d129790f7d0051d918f7a7ee4ac85d5fda2
Mritunjay Chaudhary <mkdd10@gmail.com>
2017-02-27 06:03:12 +0530

    Update Girls.java

--- q1/Girls.java
+++ q1/Girls.java
@@ -1,3 +1,4 @@
+/*! Girls class*/
 public class Girls {
 	String name;
 	int attar;
@@ -6,7 +7,7 @@ public class Girls {
 	String boyf;
 	String status;
 	
-
+/*! boolean type is_eligible function to check conditions */
 	boolean is_eligible(int budget){
 	if (this.expense <= budget) {
 			return true;
