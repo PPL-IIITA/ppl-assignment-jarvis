8204643cf11c00a55a072f6fe8c78c398feda421
Mritunjay Chaudhary <mkdd10@gmail.com>
2017-02-27 06:29:35 +0530

    Update runq1.sh

--- q1/runq1.sh
+++ q1/runq1.sh
@@ -1,7 +1,10 @@
+# checking wheather log.txt file exist or not (if already exist then removes first )
 file="log.txt"
 if [ -f $file ] ; then
 		rm $file
 fi
-javac Boys.java csv_gen.java Girls.java MyLogger.java q1.java
+# compiling java files
+javac *.java
+# passed the main class to command line argument
 java q1
 
