558191e6ee4c7f781e2b54f718cffc0366690251
Mritunjay Chaudhary <mkdd10@gmail.com>
2017-02-27 05:59:10 +0530

    Update Boys.java

--- q1/Boys.java
+++ q1/Boys.java
@@ -1,3 +1,4 @@
+/*! class Boys*/
 public class Boys {
 	String name;
 	int intelligence;
@@ -8,7 +9,7 @@ public class Boys {
 	String status;
 	
 
-
+/*! boolean type is_eligible function to check conditions */
 	boolean is_eligible(int expense,int attar){
 		if (this.budget >= expense && this.min_attr_req <= attar){
 			return true;
